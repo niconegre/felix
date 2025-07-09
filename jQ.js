1c62063c44c768180e574414bdd13941cab1497c
Nicoleta Negreanu <86585461+niconegre@users.noreply.github.com>
2025-07-09 10:31:13 -0700

    Add files via upload

---
+++ jQ.js
@@ -0,0 +1,9 @@
+$("document").ready(function(){
+$("#img1").mouseenter(function(){
+    $("#img1").css("width", "500px");
+});
+$("#img1").mouseleave(function(){
+    $("#img1").css("width","250px");
+})
+});
+
